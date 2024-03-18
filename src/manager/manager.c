c0b5435c6eb96d45c9d5deeb0340c058a4f8dfd1
Xayiide <wickedcheerio@protonmail.com>
2024-03-18 22:48:23 +0100

    Se añade inicialización de datos necesarios. Se llega hasta el bucle inicial

--- src/manager/manager.c
+++ src/manager/manager.c
@@ -1,37 +1,91 @@
-#include <stdio.h>  /* printf, perror          */
-#include <stdlib.h> /* exit, EXIT_SUCCESS, ... */
+#include <stdio.h>  /* printf, perror                */
+#include <stdlib.h> /* exit, atoi, EXIT_SUCCESS, ... */
+#include <netdb.h>  /* getaddrinfo                   */
+#include <netinet/in.h> /* sockaddr_in, INET_ADDRSTRLEN */
 
-#define PORT    3490
-#define BACKLOG 10
+#define LISTENQUEUE 10
 
-static int create_srv_socket()
+int main(int argc, char *argv[])
 {
-    int                srv_socket;
-    int                aux_res;
-    struct sockaddr_in addr;
 
-    addr.sin_family = AF_INET;
-    addr.sin_port   = htons(PORT);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    struct addrinfo     hints, *res;
+    char                yes = 1;
+    int                 ret_tmp;
+    int                 new_fd, sockfd;
+    struct sockaddr_storage their_addr;
 
-    aux_res = bind(srv_socket, (struct sockaddr *) &addr,
-                   sizeof(addr));
-    if (aux_res == -1)
-        perror("bind");
-
-    aux_res = listen(srv_socket, BACKLOG);
-    if (aux_res == -1)
-        perror("listen");
-
-    return srv_socket;
-}
-
-
-
-int main()
-{
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <name> <service>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
     printf("[Manager]\n");
 
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags    = AI_PASSIVE; /* Usar nuestra IP propia */
+
+    ret_tmp = getaddrinfo(argv[1], argv[2], &hints, &res);
+    if (ret_tmp != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n",
+                gai_strerror(ret_tmp));
+        return EXIT_FAILURE;
+    }
+
+    /* getaddrinfo nos rellena structs con información sobre el destino al que
+     * nos vamos a conectar. */
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1) {
+        fprintf(stderr, "socket\n");
+        return EXIT_FAILURE;
+    }
+
+    ret_tmp = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    if (ret_tmp == -1) {
+        fprintf(stderr, "setsockopt\n");
+        return EXIT_FAILURE;
+    }
+
+    ret_tmp = bind(sockfd, res->ai_addr, res->ai_addrlen);
+    if (ret_tmp == -1) {
+        close(sockfd);
+        fprintf(stderr, "bind\n");
+        return EXIT_FAILURE;
+    }
+
+    freeaddrinfo(res);
+
+    ret_tmp = listen(sockfd, LISTENQUEUE);
+    if (ret_tmp == -1) {
+        fprintf(stderr, "listen\n");
+        return EXIT_FAILURE;
+    }
+
+
+    printf("Waiting for connections...\n");
+    while (1) {
+        socklen_t sin_size;
+        new_fd = accept(sockfd, )
+
+    }
+
+
     return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
