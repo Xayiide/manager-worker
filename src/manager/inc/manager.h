2c6784737cb6c8e4356c37823adca50edf7fcb14
Xayiide <wickedcheerio@protonmail.com>
2024-04-08 23:20:06 +0200

    manager: se limpia manager.h quitando cosas innecesarias

--- src/manager/inc/manager.h
+++ src/manager/inc/manager.h
@@ -1,21 +1,7 @@
 #ifndef MANAGER_H_
 #define MANAGER_H_
 
-#include <stdint.h>     /* uint_t           */
-#include <netinet/in.h> /* INET6_ADDRSTRLEN */
-#include <poll.h>       /* polling          */
-
 #define CLN_NAME_MAXLEN 16
 #define MAX_CLIENTS     10
-
-typedef struct {
-    struct sockaddr_in  saddr;
-    char                ip_str[INET6_ADDRSTRLEN];
-    uint16_t            cln_port;
-    char                cln_name[CLN_NAME_MAXLEN];
-    struct pollfd      *pfd;
-} manager_conn_t;
-
-
 
 #endif
